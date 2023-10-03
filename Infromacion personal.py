0c5b5a3a618de062d723f8e27f4bb73e633a1dcf
Nain <ne.villacisd@uea.edu.ec>
2023-10-02 22:11:24 -0500

    Semana 15

--- Infromacion personal.py
+++ Infromacion personal.py
@@ -1,13 +1,13 @@
 # Crear el diccionario informacion_personal
 informacion_personal = {
-    "nombre": "Ana García",
+    "nombre": "Juan Leon",
     "edad": 25,
     "ciudad": "Ciudad Original",
     "profesion": "Ingeniera Industrial"
 }
 
 # Acceder y modificar el valor de la clave "ciudad"
-nueva_ciudad = "Ciudad Modificada"
+nueva_ciudad = "Ciudad Patate"
 informacion_personal["ciudad"] = nueva_ciudad
 
 # Agregar una nueva clave-valor para representar la "profesion"
@@ -15,7 +15,7 @@ informacion_personal["profesion"] = "Analista de Datos"
 
 # Verificar la existencia de la clave "telefono" y agregarla si no existe
 if "telefono" not in informacion_personal:
-    informacion_personal["telefono"] = "555-1234"
+    informacion_personal["telefono"] = "2831734"
 
 # Eliminar la clave "edad"
 if "edad" in informacion_personal:
