95875d88bc2b53865a38d288d93886da5c5d4d98
Nain <ne.villacisd@uea.edu.ec>
2023-10-02 22:09:16 -0500

    Semana 15

--- main.py
+++ main.py
@@ -1,36 +1,25 @@
-def calcular_temperatura_promedio(datos):
-    """
-    Calcula la temperatura promedio de cada ciudad en un período de tiempo dado.
+# Crear el diccionario informacion_personal
+informacion_personal = {
+    "nombre": "Ana García",
+    "edad": 25,
+    "ciudad": "Ciudad Original",
+    "profesion": "Ingeniera Industrial"
+}
 
-    Args:
-    datos (list): Una lista de listas que contiene los datos de temperatura para cada ciudad y semana.
+# Acceder y modificar el valor de la clave "ciudad"
+nueva_ciudad = "Ciudad Modificada"
+informacion_personal["ciudad"] = nueva_ciudad
 
-    Returns:
-    dict: Un diccionario donde las claves son el nombre de las ciudades y los valores son las temperaturas promedio.
-    """
-    temperatura_promedio_por_ciudad = {}  # Creamos un diccionario para almacenar los resultados
+# Agregar una nueva clave-valor para representar la "profesion"
+informacion_personal["profesion"] = "Analista de Datos"
 
-    for ciudad_data in datos:
-        ciudad = ciudad_data[0]  # El primer elemento de cada lista es el nombre de la ciudad
-        temperaturas_semana = ciudad_data[1:]  # El resto de elementos son temperaturas de las semanas
-        suma_temperaturas = sum(temperaturas_semana)
-        temperatura_promedio = suma_temperaturas / len(temperaturas_semana)
+# Verificar la existencia de la clave "telefono" y agregarla si no existe
+if "telefono" not in informacion_personal:
+    informacion_personal["telefono"] = "555-1234"
 
-        # Almacenamos la temperatura promedio en el diccionario
-        temperatura_promedio_por_ciudad[ciudad] = temperatura_promedio
+# Eliminar la clave "edad"
+if "edad" in informacion_personal:
+    del informacion_personal["edad"]
 
-    return temperatura_promedio_por_ciudad
-
-# Ejemplo de datos de temperaturas para 3 ciudades durante 4 semanas
-datos_temperaturas = [
-    ["Ciudad A", 25, 28, 30, 27],
-    ["Ciudad B", 22, 26, 24, 23],
-    ["Ciudad C", 30, 32, 31, 29]
-]
-
-# Llamamos a la función para calcular las temperaturas promedio
-temperaturas_promedio = calcular_temperatura_promedio(datos_temperaturas)
-
-# Imprimimos los resultados
-for ciudad, promedio in temperaturas_promedio.items():
-    print(f"La temperatura promedio en {ciudad} es {promedio} grados Celsius.")
+# Imprimir el diccionario final
+print(informacion_personal)
